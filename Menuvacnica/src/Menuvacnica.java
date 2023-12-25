d599fc6ce2a4ba055bc76b03e173bd817d048fbf
pitomacAhmed <148368787+pitomacAhmed@users.noreply.github.com>
2023-12-25 16:35:24 +0100

    Add files via upload

---
+++ Menuvacnica/src/Menuvacnica.java
@@ -0,0 +1,12 @@
+
+public class Menuvacnica {
+
+	public static void main(String[] args) {
+	double EUR = (61.7);	
+    int DEN = 69;
+    Double REZ 	= (EUR * DEN);
+    System.out.println ("ukupno denara:");
+    System.out.print (REZ);
+	}
+
+}
